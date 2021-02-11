80574e020bfcb52921f608acfedcaed2ceefc8dd
ErikaVander <erikavander123@gmail.com>
2021-02-11 13:16:16 -0800

    Made Calendar Work

---
+++ CollectionViewTesting/CalendarHelper.swift
@@ -0,0 +1,68 @@
+//
+//  TheData.swift
+//  CollectionViewTesting
+//
+//  Created by Vanderhoff on 2/10/21.
+//
+
+import Foundation
+
+var num: [String] = []
+
+let calendar = Calendar.current
+
+func plusmonth(date: Date) -> Date
+{
+    return calendar.date(byAdding: .month, value: 1, to: date)!
+}
+
+func minusMonth(date: Date) -> Date
+{
+    return calendar.date(byAdding: .month, value: -1, to: date)!
+}
+
+func numDaysInMonth(date: Date) -> Int
+{
+    let range = calendar.range(of: .day, in: .month, for: date)!
+    return range.count
+}
+
+//This was the original function used by youtuber but didn't understand how it really worked behind the scenes so I went with something else bellow.
+//
+//func firstOfMonth(date: Date) -> Date
+//{
+//    let component = calendar.dateComponents([.month, .year], from: date)
+//    return calendar.date(from: component)!
+//}
+
+func monthString(date: Date) -> String
+{
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "LLLL"
+    return dateFormatter.string(from: date)
+}
+
+func yearString(date: Date) -> String
+{
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy"
+    return dateFormatter.string(from: date)
+}
+
+func dayOfMonth(date: Date) -> Int
+{
+    let components = calendar.dateComponents([.day], from: date)
+    return components.day!
+}
+
+func weekDay(date: Date) ->  Int
+{
+    let component = calendar.dateComponents([.weekday], from: date)
+    return component.weekday! - 1
+}
+
+func firstDayOfMonth(date: Date) -> Date
+{
+    let firstDay: Date = calendar.date(bySetting: .day, value: 1, of: date)!
+    return firstDay
+}
--- CollectionViewTesting/ViewController.swift
+++ CollectionViewTesting/ViewController.swift
@@ -12,12 +12,74 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     @IBOutlet weak var collectionView: UICollectionView!
     
+    var selectedDate = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        setCellViews()
+        
+        //fillTitlesF(month: "Jan")
+        fillMonth()
+    }
+    
+    
+    
+    
+    //Functions to create the calendar and scroll to other months
+    
+    func fillMonth()
+    {
+        num.removeAll()
+        let daysInMonth = numDaysInMonth(date: selectedDate)
+        let firstDayMonth = firstDayOfMonth(date: selectedDate)
+        let startingSpaces = weekDay(date: firstDayMonth)
+        
+        var count: Int = 1
+     
+        while (count < 42)
+        {
+            if (count <= startingSpaces || count - startingSpaces > daysInMonth)
+            {
+                num.append("")
+            }
+            else
+            {
+                num.append(String(count - startingSpaces))
+            }
+            count += 1
+        }
+    }
+    
+    func fillNextMonth()
+    {
+        selectedDate = minusMonth(date: selectedDate)
+        fillMonth()
+    }
+    
+    func fillPreviousMonth()
+    {
+        selectedDate = plusmonth(date: selectedDate)
+        fillMonth()
+    }
+    
+    
+    
+    
+    
+    //Setting up the Collection view
+    
+    func setCellViews() {
+        let width = (collectionView.frame.size.width - 2)/8
+        let height = (collectionView.frame.size.height - 2)/8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+        
+        collectionView.layer.cornerRadius = 5
     }
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -25,14 +87,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return titlesF.count
+        let tempNum = num.count
+        return tempNum
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cellOne = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
         let cellIndex = indexPath.item
         
-        cellOne.label.text = titlesF[cellIndex]
+        let tempString = num[cellIndex]
+        
+        cellOne.label.text = tempString
         
         return cellOne
     }
