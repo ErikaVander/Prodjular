5d9ae3fbbc22c80dab06fb31ecdccb030c0d8781
ErikaVander <erikavander123@gmail.com>
2021-06-29 18:27:02 -0700

    CollectionView now scrolls and pages the way it's supposed to. Only problem: upon changing the direction the scroll, the program skips a month.

--- CollectionViewTesting/Model/CalendarHelper.swift
+++ CollectionViewTesting/Model/CalendarHelper.swift
@@ -66,3 +66,43 @@ func firstDayOfMonth(date: Date) -> Date
 	let firstDay: Date = calendar.nextDate(after: date, matching: DateComponents(day: 1), matchingPolicy: .nextTime, repeatedTimePolicy: .first, direction: .backward)!
     return firstDay
 }
+
+func fillMonth(parDate: Date)
+{
+	num.removeAll()
+	var x: Int = 0
+	var startDate = minusMonth(date: parDate)
+	while x < 3 {
+		print("x in loop: \(x)")
+		let daysInMonth = numDaysInMonth(date: startDate)
+		let firstDayMonth = firstDayOfMonth(date: startDate)
+		let startingSpaces = weekDay(date: firstDayMonth)
+		
+		num.append("SU")
+		num.append("MO")
+		num.append("TU")
+		num.append("WE")
+		num.append("TH")
+		num.append("FR")
+		num.append("SA")
+		
+		var count: Int = 1
+		
+		while (count < 43)
+		{
+			if (count <= startingSpaces || count - startingSpaces > daysInMonth)
+			{
+				num.append("")
+			}
+			else
+			{
+				num.append(String(count - startingSpaces))
+			}
+			count += 1
+		}
+		x  += 1
+		startDate = plusmonth(date: startDate)
+	}
+	
+	print("done with fillMonth")
+}
