a1b6479e365a8eb7afd167ad9efb2b1dabbde059
ErikaVander <erikaVander123@gmail.com>
2024-10-09 20:39:28 -0700

    Changed the Friends page layout.

---
+++ CollectionViewTesting/View/EmptyFriendsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmptyFriendsTableViewCell.swift
+//  CollectionViewTesting
+//
+//  Created by Vanderhoff on 10/9/24.
+//
+
+import UIKit
+
+class EmptyFriendsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
